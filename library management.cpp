21467db72474efb902b324ffddbbafb97b54deb7
PONNADA ANIRUDH <134695202+Anirudh504@users.noreply.github.com>
2024-09-06 14:23:16 +0530

    library management.cpp

--- library management.cpp
+++ library management.cpp
@@ -2,15 +2,11 @@
 #include <string.h>
 
 #define MAX_BOOKS 100
-
-// Structure for a book
 struct Book {
     int id;
     char title[100];
     char author[100];
 };
-
-// Function prototypes
 void addBook(struct Book books[], int *count);
 void displayBooks(struct Book books[], int count);
 int searchBook(struct Book books[], int count, int id);
@@ -18,8 +14,7 @@ int searchBook(struct Book books[], int count, int id);
 int main() {
     struct Book books[MAX_BOOKS];
     int count = 0;
-    int choice, id, index;  // Declare 'index' here, outside the switch block
-
+    int choice, id, index;  
     while (1) {
         printf("\nCenturion Library Management System\n");
         printf("1. Add Book\n");
@@ -28,7 +23,6 @@ int main() {
         printf("4. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-
         switch (choice) {
             case 1:
                 addBook(books, &count);
@@ -39,7 +33,7 @@ int main() {
             case 3:
                 printf("Enter book ID to search: ");
                 scanf("%d", &id);
-                index = searchBook(books, count, id);  // Use the 'index' declared outside
+                index = searchBook(books, count, id); 
                 if (index != -1) {
                     printf("Book found: ID: %d, Title: %s, Author: %s\n", books[index].id, books[index].title, books[index].author);
                 } else {
@@ -55,8 +49,6 @@ int main() {
 
     return 0;
 }
-
-// Function to add a book
 void addBook(struct Book books[], int *count) {
     if (*count >= MAX_BOOKS) {
         printf("Library is full, can't add more books.\n");
@@ -65,14 +57,12 @@ void addBook(struct Book books[], int *count) {
     printf("Enter book ID: ");
     scanf("%d", &books[*count].id);
     printf("Enter book title: ");
-    scanf(" %[^\n]%*c", books[*count].title);  // Allows spaces in title input
+    scanf(" %[^\n]%*c", books[*count].title); 
     printf("Enter book author: ");
     scanf(" %[^\n]%*c", books[*count].author);
     (*count)++;
     printf("Book added successfully.\n");
 }
-
-// Function to display all books
 void displayBooks(struct Book books[], int count) {
     if (count == 0) {
         printf("No books available in the library.\n");
@@ -83,14 +73,12 @@ void displayBooks(struct Book books[], int count) {
         printf("ID: %d, Title: %s, Author: %s\n", books[i].id, books[i].title, books[i].author);
     }
 }
-
-// Function to search for a book by ID
 int searchBook(struct Book books[], int count, int id) {
     for (int i = 0; i < count; i++) {
         if (books[i].id == id) {
-            return i;  // Return the index of the book
+            return i; 
         }
     }
-    return -1;  // Return -1 if the book is not found
+    return -1; 
 }
 
